4aac06c2939e3468bd4f0f77ac109eea5a789300
QiyanWang <chicowang16@gmail.com>
2018-04-17 03:06:40 -0400

    Create Direction validation methond

--- src/pers/qiyanwang/snake/Direction.java
+++ src/pers/qiyanwang/snake/Direction.java
@@ -1,32 +1,36 @@
 package pers.qiyanwang.snake;
 
+
 /**
  * 贪吃蛇前进的方向
  */
 public enum Direction {
 
-UP(0),
-RIGHT(1),
-DOWN(2),
-LEFT(3);
+    UP(0),
+    RIGHT(1),
+    DOWN(2),
+    LEFT(3);
 
-private final int directionCode;
+    private final int directionCode;
 
-Direction(int directionCode) {
-this.directionCode = directionCode;
-}
+    Direction(int directionCode) {
+        this.directionCode = directionCode;
+    }
 
-public int directionCode() {
-return directionCode;
-}
+    public int directionCode() {
+        return directionCode;
+    }
 
-/**
- * 判断方向改变是否有效，例如从向上变为向下为无效，从向上变为向左为有效
- * @param direction
- * @return
- */
-public boolean compatibleWith(Direction direction) {
+    /**
+     * 判断方向改变是否有效，例如从向上变为向下为无效，从向上变为向左为有效
+     * @param direction
+     * @return
+     */
+    public boolean compatibleWith(Direction direction) {
 // your code here
-return true;
-}
+        if(Math.abs(directionCode - direction.directionCode()) == 2){
+            return false;
+        }
+        return true;
+    }
 }
